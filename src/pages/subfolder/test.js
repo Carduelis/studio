17165a6980f9c6b2c544e225fb56b5c832a7b093
Pavel <pavepy@yandex.ru>
2020-01-04 13:37:44 +0100

    add subfolder

---
+++ src/pages/subfolder/test.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../../components/layout"
+
+const TestPage = () => (
+  <Layout>
+    <Link to="/page-2/">To page 2</Link>
+  </Layout>
+)
+
+export default TestPage
